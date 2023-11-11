6e96b6eb7e4fafad632a156ee1b924ebaaed9692
HSatish <hirolsatish@gmail.com>
2023-11-11 10:20:23 +0530

    updated

--- test_app.py
+++ test_app.py
@@ -29,9 +29,9 @@ import pytest
 class TestApp(unittest.TestCase):
 
     def setUp(self):
-        app = create_app()
-        app.config['TESTING'] = True
-        self.client = app.test_client()
+        self.app = create_app()
+        self.app.config['TESTING'] = True
+        self.client = self.app.test_client()
 
     def test_index_route(self):
         response = self.client.get('/')
@@ -46,15 +46,20 @@ class TestApp(unittest.TestCase):
 
 @pytest.mark.parametrize("browser", ["chrome", "firefox"])
 def test_selenium(browser):
-    # Replace with the actual path to your webdriver executable
-    webdriver_path = "path/to/your/webdriver/executable"
+    # Replace with the actual path to your webdriver executables
+    chrome_webdriver_path = "path/to/your/chromedriver/executable"
+    firefox_webdriver_path = "path/to/your/geckodriver/executable"
 
-    with webdriver.Chrome(executable_path=webdriver_path) as driver:
-        driver.get("http://localhost:5000")  # Replace with your app's URL
-        assert "Gender Prediction" in driver.title
-
-        # Add more Selenium tests as needed
-        # ...
+    if browser == "chrome":
+        with webdriver.Chrome(executable_path=chrome_webdriver_path) as driver:
+            driver.get("http://localhost:5000")
+            assert "Gender Prediction" in driver.title
+            # Add more Chrome-specific tests as needed
+    elif browser == "firefox":
+        with webdriver.Firefox(executable_path=firefox_webdriver_path) as driver:
+            driver.get("http://localhost:5000")
+            assert "Gender Prediction" in driver.title
+            # Add more Firefox-specific tests as needed
 
 if __name__ == '__main__':
     # If you run this file directly, it will run both unit and Selenium tests
